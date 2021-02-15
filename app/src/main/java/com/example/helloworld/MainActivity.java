bb3acf3e5c50754012dfaa0e958cf55b3fef181d
Drew Harper <dharper998@gmail.com>
2021-02-15 11:44:00 -0500

    Added button that switches hello world text from visible to invisible

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView hello = findViewById(R.id.hello);
+                if(hello.getVisibility() == View.VISIBLE){
+                    hello.setVisibility(View.INVISIBLE);
+                    button.setText("Say Hello!");
+                } else {
+                    hello.setVisibility(View.VISIBLE);
+                    button.setText("Say Goodbye");
+                }
+            }
+        });
     }
 }
