ff67028efa2b9f903203a15c5e995d87fc87ba39
Drew Harper <dharper998@gmail.com>
2021-02-17 11:08:30 -0500

    Added a progress bar to track button clicks. Added a message that displays on 100 button clicks, along with a restart button to reset all progress

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button button = findViewById(R.id.button_id);
+        final ProgressBar bar = findViewById(R.id.progressBar);
+        final Button restart = findViewById(R.id.restart);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 TextView hello = findViewById(R.id.hello);
@@ -23,7 +26,25 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     hello.setVisibility(View.VISIBLE);
                     button.setText("Say Goodbye");
+                    bar.incrementProgressBy(1);
+                    if(bar.getProgress() == 50){
+                        TextView congrats = findViewById(R.id.congrats);
+                        congrats.setVisibility(View.VISIBLE);
+                        restart.setVisibility(View.VISIBLE);
+                    }
                 }
+            }
+        });
+
+        restart.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView hello = findViewById(R.id.hello);
+                TextView congrats = findViewById(R.id.congrats);
+                hello.setVisibility(View.INVISIBLE);
+                congrats.setVisibility(View.INVISIBLE);
+                button.setText("Say Hello!");
+                bar.setProgress(0);
+                restart.setVisibility(View.INVISIBLE);
             }
         });
     }
